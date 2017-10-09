fe6db83da680fd06754b5961aa377008e4a436f7
Agne Klimaite <agnaite@me.com>
2017-10-09 16:28:21 -0700

    added user input box in view

---
+++ controllers/application_controller.rb
@@ -0,0 +1,140 @@
+require 'sinatra'
+require 'twitter'
+require 'awesome_print'
+require 'json'
+
+set :root, File.join(File.dirname(__FILE__), '..')
+set :views, Proc.new { File.join(root, "views") }
+
+get '/' do
+  username = params[:username]
+  if !username.nil?
+    @tweet_getter = TweetGetter.new(username)
+    generated_text = @tweet_getter.get_tweets!
+  else
+    generated_text = 'no user entered'
+  end
+  erb :index, :locals => {:text => generated_text}
+end
+
+class TweetGetter
+
+  def initialize username
+    @username = username
+  end
+
+  def username
+    @username
+  end
+
+  def get_tweets!
+    if has_tweet_file?
+      file = File.read('./data/'+tweet_file)
+      data_hash = JSON.parse(file)
+      make_text(data_hash)
+    else
+      tweets = scrape_twitter
+      File.open('./data/'+tweet_file, 'w') do |handle|
+        handle.puts JSON.pretty_generate(tweets)
+      end
+    end
+  end
+
+  private
+
+  def scrape_twitter
+    response = get_tweets(@username)
+    text = get_text(response)
+    chains = make_chains(text)
+  end
+
+  def has_tweet_file?
+    File.exists?('./data/'+tweet_file)
+  end
+
+  def tweet_file
+    "#{username}.json"
+  end
+
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield(max_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+
+  def get_all_tweets(user)
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key    = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+    end
+
+    collect_with_max_id do |max_id|
+      options = {count: 200, include_rts: false}
+      options[:max_id] = max_id unless max_id.nil?
+      client.user_timeline(user, options)
+    end
+  end
+
+  def get_tweets(user)
+    begin
+      get_all_tweets(user)
+    rescue Twitter::Error::TooManyRequests => error
+      sleep error.rate_limit.reset_in + 1
+      retry
+    end
+  end
+
+  def get_text(response)
+    tweets = []
+
+    for tweet in response
+      for word in tweet.text.split
+        if word[0] != '@' && !word.start_with?('http')
+          tweets.push(word)
+        end
+      end
+    end
+    tweets << nil
+  end
+
+  def make_chains(words)
+    chains = {}
+
+    for i in 0..words.length-2
+      word_1 = words[i]
+      word_2 = words[i + 1]
+      value = words[i + 2]
+
+      if !chains.include? word_1
+        chains[word_1] = {}
+      end
+      if !chains[word_1].include? word_2
+        chains[word_1][word_2] = []
+      end
+
+      chains[word_1][word_2] << value
+    end
+    chains
+  end
+
+  def make_text(chains)
+    word_1 = chains.keys.sample
+    word_2 = chains[word_1].keys.sample
+
+    words = [word_1, word_2]
+    word = chains[word_1][word_2].sample
+
+    while (chains.include? word_1) && (chains[word_1].include? word_2) && (words.length < 50)
+       word_1 = word_2
+       word_2 = word
+       words << word
+       if  chains[word_1].include? word_2
+         word = chains[word_1][word_2].sample
+       end
+    end
+
+    words.join(' ')
+  end
+
+end
+
