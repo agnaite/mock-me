63f7e3f6954ae36d670827363c12b3cd1149561a
Agne Klimaite <agnaite@me.com>
2017-10-11 18:58:41 -0700

    changed locals to instance variables and added generate again button

--- controllers/app.rb
+++ controllers/app.rb
@@ -10,33 +10,32 @@ set :views, Proc.new { File.join(root, "views") }
 
 enable :sessions
 
+
+
 get '/' do
-  username = params[:username]
+  @username = params[:username]
 
-  if !username.nil?
-    @markov = MarkovGenerator.new
-    @tweet_getter = TweetGetter.new(username)
+  if @username
+    markov = MarkovGenerator.new
+    tweet_getter = TweetGetter.new(@username)
 
-    if (!@markov.has_file?(username)) && (@tweet_getter.is_user_with_tweets?)
-      tweets = @tweet_getter.get_tweets!
-      @markov.write_file(@markov.make_chains(tweets), username)
+    if (!markov.has_file?(@username)) && (tweet_getter.is_user_with_tweets?)
+      tweets = tweet_getter.get_tweets!
+      markov.write_file(markov.make_chains(tweets), @username)
     end
 
-    if @tweet_getter.is_user_with_tweets?
-      user_color = @tweet_getter.get_color!
+    if tweet_getter.is_user_with_tweets?
+      @color = tweet_getter.get_color!
     end
 
-    if @markov.has_file?(username)
-      generated_text = @markov.generate_text(@markov.read_file(username))
+    if markov.has_file?(@username)
+      @text = markov.generate_text(markov.read_file(@username))
     else
       session[:flash] = "User does not exist or has no tweets!"
     end
   end
 
-  erb :index, :locals => {:text => generated_text || '',
-                          :user => username,
-                          :color => user_color || '000',
-                          :flash => session[:flash]}
+  erb :index
 end
 
 
